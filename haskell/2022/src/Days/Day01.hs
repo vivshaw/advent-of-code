fbca0adb3fdead1689b028e6ffd87f3e873d341a
Hannah Vivian Shaw <hvivianshaw@gmail.com>
2022-12-05 21:14:49 -0500

    turn on -Wall and refactor days 1 and 2

--- haskell/2022/src/Days/Day01.hs
+++ haskell/2022/src/Days/Day01.hs
@@ -2,14 +2,6 @@ module Days.Day01 (runDay) where
 
 {- ORMOLU_DISABLE -}
 import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
--- haskell/2022/src/Days/Day02.hs
+++ haskell/2022/src/Days/Day02.hs
@@ -1,19 +1,11 @@
 module Days.Day02 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
-import Data.Void
+import Data.Functor
+import Control.Applicative
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -23,97 +15,90 @@ runDay = R.runDay inputParser partA partB
 inputParser :: Parser Input
 inputParser = parseRound `sepBy` endOfLine
 
-parseRound :: Parser Round
+parseRound :: Parser UnresolvedGame
 parseRound = do
-  opponentMove <- toOppMove <$> satisfy (inClass "ABC")
-  space
-  myMove <- toMyMove <$> satisfy (inClass "XYZ")
-  return Round {opponentMove, myMove}
-
-toOppMove :: Char -> OppMoveSymbol
-toOppMove 'A' = A
-toOppMove 'B' = B
-toOppMove 'C' = C
-
-toMyMove :: Char -> MyMoveSymbol
-toMyMove 'X' = X
-toMyMove 'Y' = Y
-toMyMove 'Z' = Z
+  opponentMove <- char 'A' $> Rock <|> char 'B' $> Paper <|> char 'C' $> Scissors
+  _ <- space
+  myMoveSymbol <- char 'X' $> X <|> char 'Y' $> Y <|> char 'Z' $> Z
+  return UnresolvedRound {opponentMove, myMoveSymbol}
 
 ------------ TYPES ------------
-data OppMoveSymbol = A | B | C deriving (Show)
+data Move = Rock | Paper | Scissors deriving (Show)
 data MyMoveSymbol = X | Y | Z deriving (Show)
-data Round = Round
-  { opponentMove :: OppMoveSymbol
-  , myMove :: MyMoveSymbol
+data UnresolvedGame = UnresolvedRound
+  { opponentMove :: Move
+  , myMoveSymbol :: MyMoveSymbol
   } deriving (Show)
-type Input = [Round]
+type Input = [UnresolvedGame]
 
 type OutputA = Integer
 
 type OutputB = Integer
 
 ------------ PART A ------------
-data Move = Rock | Paper | Scissors
 data Outcome = Win | Tie | Lose
+data ResolvedGame = ResolvedRound
+  { myMove :: Move
+  , outcome :: Outcome
+  }
+type Strategy = UnresolvedGame -> Move
 
-play :: Move -> Move -> Outcome
-play Rock Scissors = Lose
-play Rock Paper = Win
-play Scissors Paper = Lose
-play Scissors Rock = Win
-play Paper Rock = Lose
-play Paper Scissors = Win
-play _ _ = Tie
+play :: Strategy -> UnresolvedGame -> ResolvedGame
+play myStrategy game =
+  ResolvedRound{myMove, outcome}
+  where
+    myMove = myStrategy game
+    outcome = case (opponentMove game, myMove) of
+      (Rock, Scissors) -> Lose
+      (Rock, Paper) -> Win
+      (Scissors, Paper) -> Lose
+      (Scissors, Rock) -> Win
+      (Paper, Rock) -> Lose
+      (Paper, Scissors) -> Win
+      (_, _) -> Tie
 
-oppMove :: Round -> Move
-oppMove Round{opponentMove=A} = Rock
-oppMove Round{opponentMove=B} = Paper
-oppMove Round{opponentMove=C} = Scissors
-
-myNaiveMove :: Round -> Move
-myNaiveMove Round{myMove=X} = Rock
-myNaiveMove Round{myMove=Y} = Paper
-myNaiveMove Round{myMove=Z} = Scissors
-        
-scoreNaiveGame :: Round -> Integer
-scoreNaiveGame game = scoreOutcome $ play (oppMove game) (myNaiveMove game)  
-
-scoreOutcome :: Outcome -> Integer
-scoreOutcome outcome = case outcome of
+scoreOutcome :: ResolvedGame -> Integer
+scoreOutcome game = case outcome game of
   Lose -> 0
   Tie -> 3
   Win -> 6
 
-scoreMove :: Move -> Integer
-scoreMove move = case move of
+scoreMove :: ResolvedGame -> Integer
+scoreMove game = case myMove game of
   Rock -> 1
   Paper -> 2
   Scissors -> 3
 
-scoreNaive :: Round -> Integer
-scoreNaive round = scoreMove (myNaiveMove round) + scoreNaiveGame round
+score :: Strategy -> UnresolvedGame -> Integer
+score strategy game = scoreMove resolved + scoreOutcome resolved
+  where
+    resolved = play strategy game
+
+naive :: Strategy
+naive game = case myMoveSymbol game of
+  X -> Rock
+  Y -> Paper
+  Z -> Scissors
 
 partA :: Input -> OutputA
-partA = sum . map scoreNaive
+partA = sum . map points
+  where
+    points = score naive
 
 ------------ PART B ------------
-toCorrectMove :: Round -> Move
-toCorrectMove Round{opponentMove=A, myMove=X} = Scissors
-toCorrectMove Round{opponentMove=A, myMove=Y} = Rock
-toCorrectMove Round{opponentMove=A, myMove=Z} = Paper
-toCorrectMove Round{opponentMove=B, myMove=X} = Rock
-toCorrectMove Round{opponentMove=B, myMove=Y} = Paper
-toCorrectMove Round{opponentMove=B, myMove=Z} = Scissors
-toCorrectMove Round{opponentMove=C, myMove=X} = Paper
-toCorrectMove Round{opponentMove=C, myMove=Y} = Scissors
-toCorrectMove Round{opponentMove=C, myMove=Z} = Rock
-
-scoreCorrectGame :: Round -> Integer
-scoreCorrectGame game = scoreOutcome $ play (oppMove game) (toCorrectMove game)  
-
-scoreCorrect :: Round -> Integer
-scoreCorrect round = scoreMove (toCorrectMove round) + scoreCorrectGame round
+correct :: Strategy
+correct game = case (opponentMove game, myMoveSymbol game) of
+  (Rock, X) -> Scissors
+  (Rock, Y) -> Rock
+  (Rock, Z) -> Paper
+  (Paper, X) -> Rock
+  (Paper, Y) -> Paper
+  (Paper, Z) -> Scissors
+  (Scissors, X) -> Paper
+  (Scissors, Y) -> Scissors
+  (Scissors, Z) -> Rock
 
 partB :: Input -> OutputB
-partB = sum . map scoreCorrect
+partB = sum . map points
+  where
+    points = score correct
