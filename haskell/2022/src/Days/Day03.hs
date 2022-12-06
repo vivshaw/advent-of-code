b26de5b91dd80a650264854bd8a9acac6922fdf0
Hannah Vivian Shaw <hvivianshaw@gmail.com>
2022-12-06 13:00:45 -0500

    final refactor day 3

--- haskell/2022/src/Days/Day03.hs
+++ haskell/2022/src/Days/Day03.hs
@@ -1,14 +1,12 @@
 module Days.Day03 (runDay) where
 
-{- ORMOLU_DISABLE -}
-import Data.List
+import Data.List ( intersect )
 import Data.Char (isUpper, ord)
 import Data.Attoparsec.Text
+    ( many1, sepBy, letter, endOfLine, Parser )
 import Data.List.Split (chunksOf)
 
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Foldable (Foldable(toList))
-{- ORMOLU_ENABLE -}
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
@@ -18,36 +16,40 @@ inputParser :: Parser Input
 inputParser = many1 letter `sepBy` endOfLine
 
 ------------ TYPES ------------
+
 type Rucksack = [Char]
 type Input = [Rucksack]
 
-type OutputA = Int
+type Output = Int
 
-type OutputB = Int
+type Splitter = forall a . [a] -> [[a]]
+type Joiner = forall a . (Eq a) => [[a]] -> [a]
+
+------------ UTILS -------------
+
+itemsInCommon :: Joiner
+itemsInCommon = foldr1 intersect
+
+score :: Char -> Int
+score x = ord x - (if isUpper x then 38 else 96)
+
+priority :: [[Char]] -> Int
+priority = score . head . itemsInCommon -- Wish I didn't need this `head` - there's only ever 1 item in common!
 
 ------------ PART A ------------
-intersection :: (Eq a) => [[a]] -> [a]
-intersection = foldr1 intersect
 
-decode :: Char -> Int
-decode x = ord x - (if isUpper x then 38 else 96)
-
-halve :: forall a . [a] -> [[a]]
-halve x = toList (splitAt half x)
-   where
-    half = length x `div` 2
-
-partA :: Input -> OutputA
-partA = sum . map priority
+halves :: Splitter
+halves x = chunksOf midpoint x
   where
-    priority = decode . head . intersection . halve
+    midpoint = length x `div` 2
+
+partA :: Input -> Output
+partA = sum . map (priority . halves)
 
 ------------ PART B ------------
 
-trios :: forall a . [a] -> [[a]]
+trios :: Splitter
 trios = chunksOf 3
 
-partB :: Input -> OutputB
-partB = sum . priorities
-  where
-    priorities = map (decode . head . intersection) . trios
+partB :: Input -> Output
+partB = sum . map priority . trios
